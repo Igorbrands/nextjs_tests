727ead0cbf1d90fd3ae027433981cc682d5d9c83
igorbrands <igorcantelmo@gmail.com>
2020-09-12 23:28:41 -0300

    My first nextjs website::sparkles::

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
@@ -9,9 +9,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+        <h1 className="title">Welcome to my website!</h1>
 
         <p className="description">
           Get started by editing <code>pages/index.js</code>
@@ -205,5 +203,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
